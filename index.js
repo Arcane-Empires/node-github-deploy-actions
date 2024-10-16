c9e7bd068e997ff96c71e32c68490ec542f6d2ce
drinkincode] <jonnysmith696910@gmail.com>
2024-10-16 01:55:28 -0500

    bugfix for cors error

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ import admin from 'firebase-admin';
 const app = express();
 const port = 8080;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
 app.use(express.json());
 
 const secretmanagerClient = new SecretManagerServiceClient()
